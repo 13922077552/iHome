867c52780dd6528eb8a93274929a0002a5e25216
Your Name <youremail@example.com>
2018-04-23 21:11:12 +0800

    房屋详情接⼝前端逻辑

--- iHome/static/js/ihome/detail.js
+++ iHome/static/js/ihome/detail.js
@@ -12,13 +12,8 @@ function decodeQuery(){
     }, {});
 }
 
-$(document).ready(function(){
-    // 获取详情页面要展示的房屋编号
-    var queryData = decodeQuery();
-    var houseId = queryData["id"];
 
-    // TODO: 获取该房屋的详细信息
-
+function swiper() {
     // TODO: 数据加载完毕后,需要设置幻灯片对象，开启幻灯片滚动
     var mySwiper = new Swiper ('.swiper-container', {
         loop: true,
@@ -27,4 +22,28 @@ $(document).ready(function(){
         pagination: '.swiper-pagination',
         paginationType: 'fraction'
     });
-})
+}
+
+$(document).ready(function(){
+    // 获取详情页面要展示的房屋编号
+    var queryData = decodeQuery();
+    var houseId = queryData["id"];
+
+    // http://127.0.0.1:5000/detail.html?id=6
+    // TODO: 获取该房屋的详细信息
+    $.get('/api/1.0/houses/'+houseId, function (response) {
+        if (response.errno == '0') {
+            // 渲染详情界面
+            var html_image = template('house-image-tmpl', {'img_urls':response.data.img_urls, 'price':response.data.price});
+            $('.swiper-container').html(html_image);
+
+            // 显示图片轮播
+            swiper();
+
+            var html_detail = template('house-detail-tmpl', {'house':response.data});
+            $('.detail-con').html(html_detail);
+        } else {
+            alert(response.errmsg);
+        }
+    });
+});
